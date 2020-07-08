36b1ebb14f995296e3d6fb0714de36a30fd860ae
Jasper K <48818984+Jvsper@users.noreply.github.com>
2020-07-07 19:30:14 -0700

    First Commit

---
+++ src/main/java/cst438hw2/controller/CityController.java
@@ -0,0 +1,30 @@
+package cst438hw2.controller;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import cst438hw2.domain.*;
+import cst438hw2.service.CityService;
+
+@Controller
+public class CityController {
+	
+	@Autowired
+	private CityService cityService;
+	
+	@GetMapping("/cities/{city}")
+	public String getWeather(@PathVariable("city") String cityName, Model model) {
+
+		CityInfo cityInfo = cityService.getCityInfo(cityName);
+		model.addAttribute("cityInfo", cityInfo);
+
+		return "index";
+	} 
+	
+}
